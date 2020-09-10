4aa9bcea57e6337f5ab27a5d75bb4c2d39fcd0c7
IvarK <ivarkera@gmail.com>
2020-09-10 20:23:15 +0300

    add handletargetting script

---
+++ Assets/Scripts/HandleTargetting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleTargetting : MonoBehaviour
+{
+
+    // The Target for the limb
+    public Transform ActualTarget;
+    public int Threshold = 10;
+
+    private Vector3 MoveTowards;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = (this.transform.position - ActualTarget.transform.position).sqrMagnitude;
+
+        if (distance * distance > Threshold * Threshold) {
+            MoveTowards = this.transform.position;
+            ActualTarget.transform.position = Vector3.MoveTowards(ActualTarget.transform.position, MoveTowards, 0.5f);
+        }
+    }
+}
