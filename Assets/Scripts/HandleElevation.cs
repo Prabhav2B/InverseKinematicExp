61036fee03ff65ee9b036dbedbcb7ac1bc62ce4c
prabhav2b <prabhav2b@gmail.com>
2020-09-10 20:22:15 +0530

    Basic Setup expanded

---
+++ Assets/Scripts/HandleElevation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleElevation : MonoBehaviour
+{
+    RaycastHit hit;
+    Ray ray;
+
+    [SerializeField] private LayerMask layerMask = 1>>1;
+    [SerializeField] private float elevationFromGround = 0.5f;
+
+    private void FixedUpdate()
+    {
+        ray = new Ray(this.transform.position + (Vector3.up * 0.5f), Vector3.down);
+
+
+        if (Physics.Raycast(ray, out hit, layerMask))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, hit.point.y, this.transform.position.z );
+        }
+        else
+        {
+            Debug.Log("oof");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+#if UNITY_EDITOR
+        Gizmos.DrawRay(ray);
+#endif
+    }
+
+}
