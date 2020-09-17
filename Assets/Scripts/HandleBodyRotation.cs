9e0237d329e6260a7150a89472cb6228f7b85d7d
IvarK <ivarkera@gmail.com>
2020-09-17 11:26:40 +0300

    add rotation handling of the body

---
+++ Assets/Scripts/HandleBodyRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleBodyRotation : MonoBehaviour
+{
+
+    // Order is BackLeft [0], BackRight [1], FrontLeft [2], FrontRight [3]
+    // Front is + on the Z axis, right is + on the X axis
+    [SerializeField] private Transform[] targets;
+    [SerializeField] private float rotationDamp = 0.35f;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The average y coordinate difference between back and front
+        float backFrontDifference = (((targets[2].position.y + targets[3].position.y) / 2) -
+            ((targets[0].position.y + targets[1].position.y) / 2));
+
+        // Average z distance from backlegs to frontlegs
+        float averageZDistance = (((targets[2].position.z + targets[3].position.z) / 2) -
+            ((targets[0].position.z + targets[1].position.z) / 2));
+
+        float leftRightDifference = (((targets[0].position.y + targets[2].position.y) / 2) -
+            ((targets[1].position.y + targets[3].position.y) / 2));
+
+        float averageXDistance = (((targets[0].position.x + targets[2].position.x) / 2) -
+            ((targets[1].position.x + targets[3].position.x) / 2));
+
+        this.GetComponent<Rigidbody>().MoveRotation(
+            new Quaternion(
+                -1 * Mathf.Atan(backFrontDifference / averageZDistance * rotationDamp), 
+                transform.rotation.y,
+                Mathf.Atan(leftRightDifference / averageXDistance * rotationDamp), 
+                transform.rotation.w
+            )
+        );
+    }
+}
