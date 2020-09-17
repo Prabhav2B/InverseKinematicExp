586b6a94ea71a736cfdf75825c208787becdbb76
IvarK <ivarkera@gmail.com>
2020-09-17 12:03:45 +0300

    add player object

---
+++ Assets/First person controller/Components/Jump.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [SerializeField]
+    GroundCheck groundCheck;
+    Rigidbody rigidbody;
+    public float jumpStrength = 2;
+    public event System.Action Jumped;
+
+
+    void Reset()
+    {
+        groundCheck = GetComponentInChildren<GroundCheck>();
+        if (!groundCheck)
+            groundCheck = GroundCheck.Create(transform);
+    }
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void AlterGravity()
+    {
+        //rigidbody.AddForce(-Vector3.up * 3 * jumpStrength, ForceMode.Impulse);
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetButtonDown("Jump") && groundCheck.isGrounded)
+        {
+            rigidbody.AddForce(Vector3.up * 3 * jumpStrength, ForceMode.Impulse);
+            Jumped?.Invoke();
+            Invoke("AlterGravity", 0.5f);
+        }
+    }
+}
