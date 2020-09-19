2838e52723ec42c7b081290786b36518fd708845
Prabhav Bhatnagar <prabhav2b@gmail.com>
2020-09-20 01:27:06 +0530

    added both rotations

---
+++ Assets/Scripts/Motion Related/HandleBodyElevationTest.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+
+public class HandleBodyElevationTest : MonoBehaviour
+{
+    [SerializeField] private Transform targetParent;
+    private HandleTargetting[] targets;
+
+    float meanTargetInitalElevation;
+    SpyderMovement spyderMovement;
+
+
+    private void Start()
+    {
+
+        targets = targetParent.GetComponentsInChildren<HandleTargetting>();
+
+        float sum = 0;
+        foreach (var target in targets)
+        {
+            sum += target.transform.position.y;
+        }
+
+        meanTargetInitalElevation = sum / targets.Length;
+
+        spyderMovement = GetComponent<SpyderMovement>();
+    }
+
+    private void FixedUpdate()
+    {
+        float elevation = CalculateAverageElevation();
+        spyderMovement.Elevation = elevation;
+    }
+
+    private float CalculateAverageElevation()
+    {
+        float sum = 0;
+        foreach (var target in targets)
+        {
+            sum += (target.transform.position.y - meanTargetInitalElevation);
+        }
+
+        return (sum / targets.Length);
+    }
+}
